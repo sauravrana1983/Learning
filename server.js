48d9a4d491e0f5fe127775560a0c06881e82493d
SauravRana <sauravrana1983@hotmail.com>
2017-07-17 16:52:00 +0530

    Validation

--- server.js
+++ server.js
@@ -31,11 +31,12 @@ app.get('/todos', function(req, resp){
 //Post Request
 
 app.post('/todos', function(req, res){
-    var body = req.body;
+    var body = _.pick(req.body, 'description', 'completed');
     if(!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0){
         return res.status(400).send();
     }
 
+    body.description = body.description.trim();
     body.id = todoNextID++; 
     todos.push(body);
     res.json(body);
