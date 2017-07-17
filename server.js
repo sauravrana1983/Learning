25e9495d9659db72639809ccde3273607153374e
SauravRana <sauravrana1983@hotmail.com>
2017-07-17 16:45:47 +0530

    Using Underscore

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var app = express();
+var _ = require('underscore');
+
 var PORT = process.env.PORT || 3000;
 var todos =[];
 var todoNextID = 1;
@@ -13,12 +15,7 @@ app.get('/', function(req, res){
 
 app.get('/todos/:id', function(req, res){
     var todoID= parseInt(req.params.id, 10);
-    var matchedTodo;
-    todos.forEach(function(todo) {
-        if(todoID === todo.id){
-            matchedTodo = todo;
-        }
-    });
+    var matchedTodo = _.findWhere(todos, {id: todoID});
 
     if(matchedTodo){
         res.json(matchedTodo);
@@ -35,6 +32,10 @@ app.get('/todos', function(req, resp){
 
 app.post('/todos', function(req, res){
     var body = req.body;
+    if(!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0){
+        return res.status(400).send();
+    }
+
     body.id = todoNextID++; 
     todos.push(body);
     res.json(body);
