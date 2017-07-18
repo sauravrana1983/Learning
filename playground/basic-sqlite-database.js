ebc1729d60faa0964f16646c04b1a24362e7a6e9
SauravRana <sauravrana1983@hotmail.com>
2017-07-18 12:18:00 +0530

    Sql Model

---
+++ playground/basic-sqlite-database.js
@@ -0,0 +1,42 @@
+var Sequelize = require('sequelize');
+var sequelize = new Sequelize(undefined, undefined, undefined, {
+    'dialect' : 'sqlite',
+    'storage': __dirname + '/basic-sqlite-database.sqlite'
+});
+
+var Todo = sequelize.define('todo', {
+    description:{
+        type: Sequelize.STRING,
+        allowNull: false,
+        validate:{
+            len: [1, 250]
+        }
+    },
+    completed: {
+        type: Sequelize.BOOLEAN,
+        allowNull: false,
+        defaultValue: false
+    }
+})
+
+sequelize.sync().then(function(){
+    console.log('Everything is synced');
+    Todo.create({
+        description: 'Test Task',
+        completed: false
+    }).then(function(todo){
+        return Todo.create({
+            description: 'Chained Task'
+        });
+    }).then(function(){
+        return Todo.findById(1);
+    }).then(function(todo){
+        if(todo){
+            console.log(todo.toJSON());
+        }else{
+            console.log('No to do available');
+        }
+    }).catch(function(e){
+        console.log(e);
+    });
+})
