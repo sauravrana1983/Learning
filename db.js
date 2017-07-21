65682360e6109945c89d193a0831f9d9be38131a
SauravRana <sauravrana1983@hotmail.com>
2017-07-21 12:22:30 +0530

    Updating the parameteres

--- db.js
+++ db.js
@@ -4,7 +4,7 @@ var env = process.env.NODE_ENV || 'development';
 var sequelize;
 
 if(env === 'production'){
-    sequelize = new Sequelize(process.env,DATABASE_URL, {
+    sequelize = new Sequelize(process.env.DATABASE_URL, {
         dialect: 'postgres'
     });
 } else {
