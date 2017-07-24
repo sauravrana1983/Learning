25032cdef1cbc9b074c4a39fb405be535ee8e049
SauravRana <sauravrana1983@hotmail.com>
2017-07-24 15:24:53 +0530

    Added Users:

--- server.js
+++ server.js
@@ -109,6 +109,14 @@ app.put('/todos/:id', function(req, res){
     });
 });
 
+app.post('/users', function(req, res){
+    var body = _.pick(req.body, 'email', 'password');
+    db.user.create(body).then(function(user){
+        res.json(user.toJSON());
+    }, function(e){
+        res.status(400).json(e);
+    });
+});
 
 
 db.sequelize.sync().then(function(){
